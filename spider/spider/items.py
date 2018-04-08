87b214e38361e12a15b9de4c835c5b796a184355
钱程 <201722210009@example.com>
2018-04-08 22:17:22 +0800

    apirl8

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -12,5 +12,5 @@ class SpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
+    #link = scrapy.Field()
+    #desc = scrapy.Field()
--- spider/spider/spiders/spidertest.py
+++ spider/spider/spiders/spidertest.py
@@ -1,13 +1,22 @@
 import scrapy
+from spider.items import SpiderItem
 
 class SpiderTest(scrapy.Spider):
     name = 'Stest'
-    allowed_domains = ['hupu.com']
-    start_urls = ['https://soccer.hupu.com/england/',
-    'https://soccer.hupu.com/spain/'
+    allowed_domains = ['bbs.hupu.com']
+    start_urls = ['https://bbs.hupu.com/'
     ]
 
     def parse(self,response):
-        filename = response.url.split('/')[-2]+'.html'
-        with open(filename,'wb') as f:
-            f.write(response.body)
+        for href in response.css(''):
+            url = response.urljoin(response.url,href.extract())
+            yield scrapy.Request(url,callback=self.parse_clubs)
+
+    
+    def parse_clubs(self,response):
+        for sel in response.xpath('//head'):
+            item = SpiderItem()
+            item['title']=sel.xpath('title/text()').extract()
+            #item['link']=sel.xpath().extract()
+            #item['desc']=sel.xpath().extract()
+            yield item
